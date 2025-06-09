e853a9458bb6b96bb06fe2fefdc21d47c49121bd
jiaochunxiao <jcx13627607951@gmail.com>
2025-06-09 09:42:26 +0800

    feat: import

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,9 @@
+/**
+ * @type {import('postcss').ProcessOptions}
+ */
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ]
-} 
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
